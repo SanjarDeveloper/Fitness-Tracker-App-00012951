f4c11bab9c48be3e03a4b40886289e69caa10590
00012951 <developer.sanjar@gmail.com>
2024-03-19 15:11:34 +0500

    Add Workout, WorkoutType models

---
+++ Fitness-Tracker-App-00012951/Models/Workout.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fitness_Tracker_App_00012951.Models
+{
+    public class Workout
+    {
+        [Required]
+        public int ID { get; set; }
+        
+        [Required(ErrorMessage = "Duration is required")]
+        public double? Duration { get; set; }
+        
+        [Required(ErrorMessage = "Burned Calories is required")]
+        public double? Calories_burned{ get; set; }
+        
+        public int? WorkoutTypeID{ get; set; }
+        
+        [ForeignKey("WorkoutTypeID")]
+        public WorkoutType? WorkoutType{ get; set; }
+
+
+    }
+}
---
+++ Fitness-Tracker-App-00012951/Models/WorkoutType.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fitness_Tracker_App_00012951.Models
+{
+    public class WorkoutType
+    {
+        public int ID { get; set; }
+        private string _name ;
+        [Required(ErrorMessage = "Name is required")]
+        public string Name 
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Name cannot be null or empty.");
+                }
+                _name = value;
+            } 
+        }
+    }
+}
