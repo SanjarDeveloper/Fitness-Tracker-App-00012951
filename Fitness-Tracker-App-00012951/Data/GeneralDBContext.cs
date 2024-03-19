0915dff66d55ab4d40f05a51d7c5f3cfc4ed4382
00012951 <developer.sanjar@gmail.com>
2024-03-19 15:13:16 +0500

    Add GeneralDBContext

---
+++ Fitness-Tracker-App-00012951/Data/GeneralDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Fitness_Tracker_App_00012951.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fitness_Tracker_App_00012951.Data
+{
+    public class GeneralDBContext:DbContext
+    {
+        public GeneralDBContext(DbContextOptions<GeneralDBContext> options) : base(options) { }
+        public DbSet<Workout> Workouts { get; set; }
+        public DbSet<WorkoutType> WorkoutTypes { get; set; }
+    }
+}
